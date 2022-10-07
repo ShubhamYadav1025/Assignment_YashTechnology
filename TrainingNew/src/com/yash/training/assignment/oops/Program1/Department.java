bc2958bb9ee558de17399e0f40801a8a28de7df6
shubham.yadav <shubham.yadav@YI1015837LT.Yash.com>
2022-10-07 23:38:12 +0530

    modify

--- TrainingNew/src/com/yash/training/assignment/oops/Program1/Department.java
+++ TrainingNew/src/com/yash/training/assignment/oops/Program1/Department.java
@@ -13,12 +13,13 @@ public class Department {
 	public int getDeptid() {
 		return deptid;
 	}
+	
 	public void setDeptid(int deptid) {
 		this.deptid = deptid; 
 	}
 	public String getDname() {
 		return dname;
-	}
+	} 
 	public void setDname(String dname) {
 		this.dname = dname;
 	}
